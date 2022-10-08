3499e466f49b2e57176d5486d9143a43b2a9c9a9
Roxas57 <juanmariacastillogimenez@gmail.com>
2022-10-08 19:18:34 +0200

    20221008 19:18 juanma

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -3,6 +3,8 @@ package controller;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javafx.scene.control.Alert;
+
 public class Controller {
 	
 	public static boolean validNick(String nick) {
@@ -11,6 +13,20 @@ public class Controller {
 		Matcher m = p.matcher(nick);
 		
 		return m.matches();
+	}
+	
+	public static void error (String header, String context) {
+		Alert alert = new Alert(Alert.AlertType.ERROR);
+		alert.setTitle("Error");
+		alert.setHeaderText(header);
+		alert.setContentText(context);
+		alert.showAndWait();
+	}
+	
+	public static void info (String header) {
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setHeaderText(header);
+		alert.show();
 	}
 
 }
